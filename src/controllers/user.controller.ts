231e10f71f03cefb884a7af8239fffc0c2307747
javierpb275 <javierpb275@gmail.com>
2022-07-19 21:28:05 +0200

    methods getUserById, getUsers, deleteProfile and refreshToken created. Also added new routes to user router

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -8,10 +8,56 @@ import config from "../config/config";
 import { pool } from "../db/postgres";
 import { QueryResult } from "pg";
 import { isCorrectEmail, isSecurePassword } from "../helpers/validation.helper";
+import { IPayload } from "../middlewares/auth.middleware";
+import jwt from "jsonwebtoken";
 
 let refreshTokens: string[] = [];
 
 export default class UserController {
+  public static async refreshToken(
+    req: Request,
+    res: Response
+  ): Promise<Response> {
+    try {
+      if (!req.body.refreshToken) {
+        return res
+          .status(400)
+          .send({ error: true, data: "Please, provide refresh token." });
+      }
+      if (!refreshTokens.includes(req.body.refreshToken)) {
+        return res
+          .status(400)
+          .send({ error: true, data: "Refresh token invalid." });
+      }
+      refreshTokens = refreshTokens.filter(
+        (reToken) => reToken != req.body.refreshToken
+      );
+
+      const payload = jwt.decode(req.body.refreshToken) as IPayload;
+
+      const accessToken: string = generateToken(
+        payload.id,
+        config.AUTH.ACCESS_TOKEN_SECRET,
+        config.AUTH.ACCESS_TOKEN_EXPIRATION
+      );
+      const refreshToken: string = generateToken(
+        payload.id,
+        config.AUTH.REFRESH_TOKEN_SECRET,
+        config.AUTH.REFRESH_TOKEN_EXPIRATION
+      );
+      refreshTokens.push(refreshToken);
+      return res.status(200).send({
+        error: false,
+        data: {
+          accessToken,
+          refreshToken,
+        },
+      });
+    } catch (err) {
+      return res.status(500).send({ error: true, data: err });
+    }
+  }
+
   public static async signUp(req: Request, res: Response): Promise<Response> {
     const { username, user_email, user_password } = req.body;
     if (!username) {
@@ -93,7 +139,7 @@ export default class UserController {
         "SELECT user_id, username, user_email, user_password FROM users WHERE user_email = $1",
         [user_email]
       );
-      if (response.rowCount === 0) {
+      if (response.rowCount <= 0) {
         return res
           .status(404)
           .send({ error: true, data: "Wrong credentials." });
@@ -167,13 +213,78 @@ export default class UserController {
         "SELECT user_id, username, user_email FROM users WHERE user_id = $1",
         [user_id]
       );
-      if (response.rowCount === 0) {
+      if (response.rowCount <= 0) {
         return res.status(404).send({ error: true, data: "User Not found." });
       }
       return res.status(200).send({
         error: false,
         data: response.rows[0],
       });
+    } catch (err) {
+      return res.status(500).send({ error: true, data: err });
+    }
+  }
+
+  public static async deleteProfile(
+    req: Request,
+    res: Response
+  ): Promise<Response> {
+    const { user_id } = req;
+    if (!req.body.refreshToken) {
+      return res
+        .status(400)
+        .send({ error: true, data: "Please, provide refresh token." });
+    }
+    if (!refreshTokens.includes(req.body.refreshToken)) {
+      return res
+        .status(400)
+        .send({ error: true, data: "Refresh token invalid." });
+    }
+
+    try {
+      const response: QueryResult = await pool.query(
+        "DELETE FROM users WHERE user_id = $1 RETURNING username, user_email",
+        [user_id]
+      );
+      if (response.rowCount <= 0) {
+        return res.status(404).send({ error: true, data: "User Not found." });
+      }
+      refreshTokens = refreshTokens.filter(
+        (reToken) => reToken != req.body.refreshToken
+      );
+      return res
+        .status(200)
+        .send({ error: false, data: { user: response.rows[0] } });
+    } catch (err) {
+      return res.status(500).send({ error: true, data: err });
+    }
+  }
+
+  public static async getUsers(req: Request, res: Response): Promise<Response> {
+    try {
+      const response: QueryResult = await pool.query(
+        "SELECT username, user_email FROM users"
+      );
+      return res.status(200).send(response.rows);
+    } catch (err) {
+      return res.status(500).send({ error: true, data: err });
+    }
+  }
+
+  public static async getUserById(
+    req: Request,
+    res: Response
+  ): Promise<Response> {
+    const { params } = req;
+    try {
+      const response: QueryResult = await pool.query(
+        "SELECT username, user_email FROM users WHERE user_id = $1",
+        [params.id]
+      );
+      if (response.rowCount <= 0) {
+        return res.status(404).send({ error: true, data: "User Not found." });
+      }
+      return res.status(200).send(response.rows[0]);
     } catch (err) {
       return res.status(500).send({ error: true, data: err });
     }
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -4,10 +4,14 @@ import { auth } from "../middlewares/auth.middleware";
 
 const router: Router = Router();
 
-//router.post("/refreshToken", UserController.refreshToken);
+router.post("/refreshToken", UserController.refreshToken);
 router.post("/", UserController.signUp);
 router.post("/signin", UserController.signIn);
 router.post("/signout", auth, UserController.signOut);
 router.get("/me", auth, UserController.getProfile);
+router.delete("/me", auth, UserController.deleteProfile);
+//router.patch("/me", auth, UserController.updateProfile);
+router.get("/", auth, UserController.getUsers);
+router.get("/:id", auth, UserController.getUserById);
 
 export default router;
